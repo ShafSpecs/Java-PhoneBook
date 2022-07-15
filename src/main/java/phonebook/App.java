1c9f8105cf8098ac105bd262cf2bceb0f5142a56
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-07-15 20:42:52 +0100

    Added `catch` methods

--- src/main/java/phonebook/App.java
+++ src/main/java/phonebook/App.java
@@ -3,7 +3,15 @@ package phonebook;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        Runner.kickStart();
+    public static void main(String[] args) {
+        try {
+            Runner.kickStart();
+        } catch (IOException err) {
+            err.printStackTrace();
+        } catch (NoSuchFieldException err) {
+            err.printStackTrace();
+        } catch (InterruptedException err) {
+            err.printStackTrace();
+        }
     }
 }
