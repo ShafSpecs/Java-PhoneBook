a12b9bb200303418942f7e234aed108f6f0fe440
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-07-15 20:43:40 +0100

    Fixed Apps' utils

--- src/main/java/phonebook/Runner.java
+++ src/main/java/phonebook/Runner.java
@@ -15,6 +15,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Runner {
     private static final Scanner input = new Scanner(System.in);
@@ -24,7 +25,7 @@ public class Runner {
     private static Session authSession = Session.LOGGED_OUT;
     private static String currentSession = null;
 
-    public static void kickStart() throws IOException {
+    public static void kickStart() throws IOException, NoSuchFieldException, InterruptedException {
         startDatabase();
 
         System.out.print("""
@@ -34,7 +35,7 @@ public class Runner {
                 1. Create an account
                 2. Login to your account
                                 
-                >>> """);
+                >>>  """);
 
         int menuChoice = input.nextInt();
 
@@ -54,7 +55,7 @@ public class Runner {
 
     // todo: Re-navigate user if logged in.
 
-    public static void startDatabase() throws IOException {
+    public static void startDatabase() {
         try {
             if (db.createNewFile()) {
                 try (FileWriter file = new FileWriter(db)) {
@@ -116,7 +117,7 @@ public class Runner {
 
     //todo: Add handler for duplicates (account)
 
-    public static void createAccount() {
+    public static void createAccount() throws NoSuchFieldException, IOException, InterruptedException {
         System.out.print("Enter your username: ");
         String name = input.next();
 
@@ -154,7 +155,7 @@ public class Runner {
         }
     }
 
-    public static void logIn() {
+    public static void logIn() throws NoSuchFieldException, IOException, InterruptedException {
         System.out.print("Enter your username: ");
         String name = input.next();
 
@@ -193,7 +194,167 @@ public class Runner {
         logIn();
     }
 
-    public static void dashboard(User user) {
-        System.out.printf("Welcome to %s's Dashboard! What do you want to do today?\n\n1. View all your contacts\n2. Add a contact\n3. Edit a contact\n4. Delete a contact\n5. Search for a contact\n6. Sign Out", user.getUserName());
+    public static void dashboard(User user) throws NoSuchFieldException, IOException, InterruptedException {
+        System.out.printf("Welcome to %s's Dashboard! What do you want to do today?\n\n1. View all your contacts\n2. Add a contact\n3. Edit a contact\n4. Delete a contact\n5. Search for a contact\n6. Sign Out\n\n>>> ", user.getUserName());
+        int userChoice = input.nextInt();
+
+        switch (userChoice) {
+            case 1 -> viewAllContacts(user);
+
+            case 2 -> addContact(user);
+
+            case 3 -> {
+                System.out.println();
+                System.out.print("Enter the contact number to edit: ");
+
+                String number = input.nextLine();
+
+                Contact foundNumber = user.getPhoneBook().searchByNumber(number);
+
+                editContact(user, foundNumber);
+            }
+
+            case 4 -> {
+                System.out.println();
+                System.out.print("Enter the contact number to delete: ");
+
+                String number = input.nextLine();
+
+                Contact foundNumber = user.getPhoneBook().searchByNumber(number);
+
+                deleteContact(user, foundNumber);
+            }
+
+            case 6 -> logOut(user);
+        }
+    }
+
+    public static void viewAllContacts(User user) throws NoSuchFieldException, IOException, InterruptedException {
+        ArrayList<Contact> phone = user.getPhoneBook().getPhoneBook();
+
+        if(phone.size() == 0) {
+            System.out.println();
+            System.out.println("No contact found!");
+            System.out.println();
+            System.out.print("1. Add a contact\n2. Go back to main menu\n3. Sign Out\n>>> ");
+
+            int userChoice = input.nextInt();
+
+            switch (userChoice) {
+                case 1 -> addContact(user);
+
+                case 2 -> dashboard(user);
+
+                case 3 -> logOut(user);
+            }
+        } else {
+            for (int i = 0; i < phone.size(); i++) {
+                System.out.println("==========");
+//                System.out.println();
+                System.out.printf("__%d__\nName: %s\nPhone Number: %s\nEmail Address: %s\nHome Address: %s\n", i + 1, phone.get(i).getName(), phone.get(i).getNumber(), phone.get(i).getEmail(), phone.get(i).getAddress());
+                System.out.println();
+                System.out.println("==========");
+            }
+
+            System.out.println();
+            System.out.print("1. Add Contact\n2. Edit Contact\n3. Delete Contact\n4. Go Back to main menu\n5. Sign Out\n>>> ");
+
+            int userChoice = input.nextInt();
+
+            switch (userChoice) {
+                case 1 -> addContact(user);
+
+                case 2 -> {
+                    System.out.println();
+
+                    System.out.print("Which contact do you want to edit(Enter the index): ");
+                    int choice = input.nextInt();
+
+                    editContact(user, phone.get(choice - 1));
+                }
+
+                case 3 -> {
+                    System.out.println();
+
+                    System.out.print("Which contact do you want to delete(Enter the index): ");
+                    int choice = input.nextInt();
+
+                    deleteContact(user, phone.get(choice - 1));
+                }
+
+                case 4 -> dashboard(user);
+
+                case 5 -> logOut(user);
+
+                default -> {
+                    System.out.println("Wrong input!");
+                    viewAllContacts(user);
+                }
+            }
+        }
+    }
+
+    public static void addContact(User user) throws NoSuchFieldException, IOException, InterruptedException {
+        System.out.println();
+        System.out.print("Enter the name (Not required. 's' to skip): ");
+        String name = input.next();
+
+        System.out.print("Enter the number (required): ");
+        String num = input.next();
+
+        System.out.print("Enter Email Address (Not required. 's' to skip): ");
+        String email = input.next();
+
+        System.out.print("Enter Home Address (Not required. 's' to skip): ");
+        String address = input.next();
+
+        user.getPhoneBook().createUser(name.equalsIgnoreCase("s") ? null : name, num, email.equalsIgnoreCase("s") ? null : email, address.equalsIgnoreCase("s") ? null : address);
+
+        writeDatabase(users);
+
+        System.out.println("User successfully created!\n");
+        dashboard(user);
+    }
+
+    public static void editContact(User user, Contact con) throws NoSuchFieldException, IOException, InterruptedException {
+        System.out.printf("Enter contacts' name (Press 's' to skip)(%s): ");
+        String name = input.next();
+
+        System.out.printf("Enter the number (Press 's' to skip)(%s): ");
+        String num = input.next();
+
+        System.out.printf("Enter Email Address (Press 's' to skip)(%s): ");
+        String email = input.next();
+
+        System.out.printf("Enter Home Address (Press 's' to skip)(%s): ");
+        String address = input.next();
+
+        con.editContact(name.equalsIgnoreCase("s") ? con.getName() : name, num.equalsIgnoreCase("s") ? con.getNumber() : num, email.equalsIgnoreCase("s") ? con.getEmail() : email, address.equalsIgnoreCase("s") ? con.getAddress() : address);
+
+        writeDatabase(users);
+        System.out.println("Contact updated successfully!");
+
+        dashboard(user);
+    }
+
+    public static void deleteContact(User user, Contact con) throws NoSuchFieldException, IOException, InterruptedException {
+        user.getPhoneBook().deleteUser(con);
+
+        writeDatabase(users);
+
+        dashboard(user);
+    }
+
+    public static void logOut(User user) throws InterruptedException, IOException, NoSuchFieldException {
+        System.out.println();
+
+        System.out.println("Logging Out...");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Successfully logged out!");
+
+        currentSession = null;
+        authSession = Session.LOGGED_OUT;
+
+        kickStart();
     }
 }
